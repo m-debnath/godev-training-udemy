c1a85df98c4edb59b29a5501e2f99be2efc5fccb
Mukul Debnath <debnath.mukul@gmail.com>
2018-10-23 02:24:21 +0530

    ninja level 12

---
+++ ninja-12-1/dog/main.go
@@ -0,0 +1,7 @@
+// Package dog provides various information about human and dog interactions.
+package dog
+
+// Years turns human years into dog years.
+func Years(years int) int {
+	return years * 7
+}
---
+++ ninja-12-1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/m-debnath/godev-training-udemy/ninja-12-1/dog"
+)
+
+func main() {
+	years := 3
+	fmt.Println(years, "human years are", dog.Years(3), "dog years")
+}
